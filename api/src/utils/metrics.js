59a42c5cc84078d5c266220f76fec12a0416a521
BridMoulin <thomas.kr@ictpe.fr>
2025-12-19 15:51:15 +0100

    fix(monitoring)

---
+++ api/src/utils/metrics.js
@@ -0,0 +1,13 @@
+import client from "prom-client";
+
+export const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+export const httpRequestDuration = new client.Histogram({
+  name: "http_request_duration_seconds",
+  help: "Duration of HTTP requests in seconds",
+  labelNames: ["method", "route", "status_code"],
+  buckets: [0.01, 0.05, 0.1, 0.3, 0.5, 1, 2, 5],
+});
+
+register.registerMetric(httpRequestDuration);
